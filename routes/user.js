bf40c3e0b5d494fe94cfddcdb016dea532b9fa1e
Nguyen Hoang Nam <nhnam010@gmail.com>
2022-10-28 07:10:04 +0700

    Add User model, controller, router

--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 // Import Controller
-const {allUser, getUserByID, store, update, destroy} = require('../app/controllers/UserController');
+const {allUser, getUserByID, store, update} = require('../app/controllers/UserController');
 
 router.get('/', allUser);
 router.post('/', store);
 router.get('/:id', getUserByID);
 router.put('/:id', update);
-router.delete('/:id', destroy);
+// router.delete('/:id', destroy);
 
 module.exports = router;
