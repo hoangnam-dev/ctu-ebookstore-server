2cc24ecc5d4c894879987438fdcb5cecf41e5eba
Nguyen Hoang Nam <nhnam010@gmail.com>
2022-10-30 06:03:12 +0700

    Update cors

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const cors = require('cors');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 const router = require('./routes');
 
 const app = express();
+
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}
+app.use(cors(corsOptions));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'resources', 'views'));
