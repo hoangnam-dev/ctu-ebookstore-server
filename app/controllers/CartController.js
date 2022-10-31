c98b5f03667ea7cf36e282ade8fb63f7a6f8e137
Nguyen Hoang Nam <nhnam010@gmail.com>
2022-10-31 09:01:47 +0700

    Add Cart controller

---
+++ app/controllers/CartController.js
@@ -0,0 +1,13 @@
+// Search authors
+const search = function (req, res) {
+  res.json({
+    error: true,
+    statusCode: 0,
+    message: "Card controller",
+  });
+};
+
+
+module.exports = {
+  search
+}
