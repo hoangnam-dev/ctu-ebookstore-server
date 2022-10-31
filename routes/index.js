e9fa8921bf80eede210540c3ae0b7c38b0833d75
Nguyen Hoang Nam <nhnam010@gmail.com>
2022-10-31 09:02:55 +0700

    Change API url

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const authorRouter = require('./author');
+const cartRouter = require('./cart');
 const commentRouter = require('./comment');
 const customerRouter = require('./customer');
 const directoryRouter = require('./directory');
@@ -20,60 +21,63 @@ const wardRouter = require('./ward');
 
 function route(app) {
     // Author routes
-    app.use('/authors', authorRouter);
+    app.use('/api/authors', authorRouter);
+    
+    // Carts routes
+    app.use('/api/carts', cartRouter);
     
     // Comment routes
-    app.use('/comments', commentRouter);
+    app.use('/api/comments', commentRouter);
     
     // Customer routes
-    app.use('/customrs', customerRouter);
+    app.use('/api/customrs', customerRouter);
     
     // Directory routes
-    app.use('/directories', directoryRouter);
+    app.use('/api/directories', directoryRouter);
 
     // District routes
-    app.use('/districts', districtRouter);
+    app.use('/api/districts', districtRouter);
     
     // Ebook routes
-    app.use('/ebooks', ebookRouter);
+    app.use('/api/ebooks', ebookRouter);
     
     // InputInffo routes
-    app.use('/inputinfos', inputInfoRouter);
+    app.use('/api/inputinfos', inputInfoRouter);
     
     // License routes
-    app.use('/licenses', licenseRouter);
+    app.use('/api/licenses', licenseRouter);
     
     // Order routes
-    // app.use('/orders', orderRouter);
+    // app.use('/api/orders', orderRouter);
     
     // OutputInfo routes
-    app.use('/outputinfos', outputInfoRouter);
+    app.use('/api/outputinfos', outputInfoRouter);
     
     // Payment routes
-    app.use('/payments', paymentRouter);
+    app.use('/api/payments', paymentRouter);
     
     // PaymentType routes
-    app.use('/paymenttypes', paymentTypeRouter);
+    app.use('/api/paymenttypes', paymentTypeRouter);
 
     // Permission routes
-    app.use('/permissions', permissionRouter);
+    app.use('/api/permissions', permissionRouter);
 
     // Province routes
-    app.use('/provinces', provinceRouter);
+    app.use('/api/provinces', provinceRouter);
 
     // Role routes
-    app.use('/roles', roleRouter);
+    app.use('/api/roles', roleRouter);
 
     // Sale routes
-    app.use('/sales', saleRouter);
+    app.use('/api/sales', saleRouter);
 
     // Supplier routes
-    app.use('/suppliers', supplierRouter);
+    app.use('/api/suppliers', supplierRouter);
 
     // User routes
-    app.use('/users', userRouter);
+    app.use('/api/users', userRouter);
 
     // Ward routes
-    app.use('/wards', wardRouter);
+    app.use('/api/wards', wardRouter);
 }
 module.exports = route;
