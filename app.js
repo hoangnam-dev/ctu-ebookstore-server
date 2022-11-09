e4467a0f82a5952d8595443afac2dee7107268b7
Nguyen Hoang Nam <nhnam010@gmail.com>
2022-11-09 09:20:39 +0700

    Config get form data

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-const cors = require('cors');
-const createError = require('http-errors');
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const bodyParser = require('body-parser');
 const router = require('./routes');
 
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+// parse application/json
+app.use(bodyParser.json({limit: '50mb'}));
+
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 const corsOptions ={
-  origin:'*', 
+  origin:'http://localhost:3001', 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200,
-  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }
 app.use(cors(corsOptions));
 
@@ -22,28 +29,8 @@ app.set('views', path.join(__dirname, 'resources', 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Router
 router(app);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
