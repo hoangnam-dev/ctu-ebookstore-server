7a5d92290b8b7e339e07398e62e6cc651ec110e2
Nguyen Hoang Nam <nhnam010@gmail.com>
2022-11-18 23:00:18 +0700

    User Auth: add jwt config

--- app.js
+++ app.js
@@ -15,15 +15,16 @@ app.use(bodyParser.json({limit: '50mb'}));
 
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'public')));
 
 const corsOptions ={
-  origin: '*', 
+  origin: ["http://localhost:3001"], 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200,
 }
 app.use(cors(corsOptions));
 
+// Set public path
+app.use(express.static(path.join(__dirname, 'public')));
 // view engine setup
 app.set('views', path.join(__dirname, 'resources', 'views'));
 app.set('view engine', 'ejs');
