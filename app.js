11c31693504b927f621d6a524ca3bad10c5b9b97
Nguyen Hoang Nam <nhnam010@gmail.com>
2022-11-19 07:00:13 +0700

    User Auth: add jwt config

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const router = require('./routes');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const router = require("./routes");
 
 const app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 // parse application/json
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.json({ limit: "50mb" }));
 
 app.use(cookieParser());
 
-
-const corsOptions ={
-  origin: ["http://localhost:3001"], 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-}
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 
 // Set public path
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 // view engine setup
-app.set('views', path.join(__dirname, 'resources', 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "resources", "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 // Router
 router(app);
